8f2bee62d98706665f6722cc64d59c2d2f1004da
rarisonr <irinararison@gmail.com>
2023-12-18 01:00:06 +0100

    Update annexe

--- ElectionSimulator.java
+++ ElectionSimulator.java
@@ -5,109 +5,102 @@ import java.util.List;
 import java.util.Map;
 
 public class ElectionSimulator {
+    public static class Votant {
+        private List<String> preferences;
+
+        public Votant(List<String> preferences) {
+            this.preferences = preferences;
+        }
+
+        public List<String> getPreferences() {
+            return preferences;
+        }
+    }
 
     public static void main(String[] args) {
-        // Exemple avec trois candidats A, B, C et un grand nombre de votants
         List<String> candidates = List.of("A", "B", "C");
-        int numVoters = 500000; // Simulation avec un grand nombre de votants
+        int numVoters = 60;
 
-        
-        // Simuler un scrutin aléatoire pour chaque votant
-        List<List<String>> ballots = simulateRandomVotes(candidates, numVoters);
-        
-        List<String> prefGlobal = calculateGlobalPreferences(candidates, numVoters, ballots);
-        System.out.println(" Voici la prederence Global selon les votants ");
-        for(String a : prefGlobal) System.out.print(a+" ");
-        System.out.println();
+        List<Votant> voters = simulateVoters(candidates, numVoters);
 
-        // Calculer le gagnant selon le critère de Condorcet
-        String condorcetWinner = findCondorcetWinner(candidates, numVoters, ballots);
+        List<String> prefGlobal = calculateGlobalPreferences(candidates, numVoters, voters);
+        System.out.println("Voici la préférence globale selon les votants : " + prefGlobal);
 
-        // Afficher les résultats
+        String condorcetWinner = findCondorcetWinner(candidates, numVoters, voters);
+
         System.out.println("Gagnant selon le critère de Condorcet : " + condorcetWinner);
     }
 
-    // Simuler un vote aléatoire pour chaque votant
-    private static List<List<String>> simulateRandomVotes(List<String> candidates, int numVoters) {
-        List<List<String>> ballots = new ArrayList<>();
+    private static List<Votant> simulateVoters(List<String> candidates, int numVoters) {
+        List<Votant> voters = new ArrayList<>();
 
         for (int i = 0; i < numVoters; i++) {
-            List<String> ballot = new ArrayList<>(candidates);
-            Collections.shuffle(ballot);
-            ballots.add(ballot);
+            List<String> preferences = new ArrayList<>(candidates);
+            Collections.shuffle(preferences);
+            voters.add(new Votant(preferences));
         }
 
-        return ballots;
+        return voters;
     }
 
-    // Trouver le gagnant selon le critère de Condorcet
-    private static String findCondorcetWinner(List<String> candidates, int numVoters, List<List<String>> ballots) {
-        // Initialiser un tableau pour stocker les victoires de chaque candidat contre les autres
-        Map<String, Integer> wins = new HashMap<>();
-        for (String candidate : candidates) {
-            wins.put(candidate, 0);
-        }
-
-        // Comparer chaque paire de candidats pour chaque votant
-        for (int i = 0; i < numVoters; i++) {
-            for (String candidate1 : candidates) {
-                for (String candidate2 : candidates) {
-                    if (!candidate1.equals(candidate2)) {
-                        List<String> currentBallot = ballots.get(i);
-                        compareCandidatesAndUpdateWins(candidate1, candidate2, currentBallot, wins);
-                    }
-                }
-            }
-        }
-
-        // Affichage des votes de chaque candidat selon les préférences
-        for (String cand : candidates) {
-            System.out.println("Nombre de votes pour " + cand + ": " + wins.get(cand));
-        }
-        
-        
-        // Trouver le candidat avec le plus grand nombre de victoires
-        return wins.entrySet().stream()
-                .max(Map.Entry.comparingByValue())
-                .map(Map.Entry::getKey)
-                .orElse(null);
-    }
-
-    // Comparer les positions des candidats dans le vote et mettre à jour les victoires
-    private static void compareCandidatesAndUpdateWins(String candidate1, String candidate2, List<String> ballot,
-                                                       Map<String, Integer> wins) {
-        int index1 = ballot.indexOf(candidate1);
-        int index2 = ballot.indexOf(candidate2);
-
-        // Si candidate1 est préféré à candidate2, incrémenter winsForCandidate1
-        // Sinon, incrémenter winsForCandidate2
-        if (index1 < index2) {
-            wins.put(candidate1, wins.get(candidate1) + 1);
-        } else {
-            wins.put(candidate2, wins.get(candidate2) + 1);
-        }
-    }
-
-     // Fonction pour calculer la liste de préférences globale
-     private static List<String> calculateGlobalPreferences(List<String> candidates, int numVoters, List<List<String>> votersPreferences) {
-        // Initialiser le tableau des points pour chaque candidat
+    private static List<String> calculateGlobalPreferences(List<String> candidates, int numVoters, List<Votant> voters) {
         Map<String, Integer> points = new HashMap<>();
+
         for (String candidate : candidates) {
             points.put(candidate, 0);
         }
 
-        // Parcourir les préférences de chaque votant et attribuer des points
-        for (List<String> voterPreferences : votersPreferences) {
+        for (Votant voter : voters) {
+            List<String> voterPreferences = voter.getPreferences();
             for (int i = 0; i < candidates.size(); i++) {
                 String candidate = voterPreferences.get(i);
                 points.put(candidate, points.get(candidate) + (numVoters - i));
             }
         }
 
-        // Trier les candidats en fonction du nombre de points
         List<String> globalPreferences = new ArrayList<>(candidates);
         globalPreferences.sort((c1, c2) -> Integer.compare(points.get(c2), points.get(c1)));
 
         return globalPreferences;
+    }
+
+    private static String findCondorcetWinner(List<String> candidates, int numVoters, List<Votant> voters) {
+        Map<String, Integer> wins = new HashMap<>();
+
+        for (String candidate : candidates) {
+            wins.put(candidate, 0);
+        }
+
+        for (Votant voter : voters) {
+            List<String> voterPreferences = voter.getPreferences();
+            for (String candidate1 : candidates) {
+                for (String candidate2 : candidates) {
+                    if (!candidate1.equals(candidate2)) {
+                        compareCandidatesAndUpdateWins(candidate1, candidate2, voterPreferences, wins);
+                    }
+                }
+            }
+        }
+
+        for (String cand : candidates) {
+            System.out.println("Nombre de votes pour " + cand + ": " + wins.get(cand));
+        }
+
+        return wins.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null);
+    }
+
+    private static void compareCandidatesAndUpdateWins(String candidate1, String candidate2, List<String> preferences,
+                                                       Map<String, Integer> wins) {
+        int index1 = preferences.indexOf(candidate1);
+        int index2 = preferences.indexOf(candidate2);
+
+        if (index1 < index2) {
+            wins.put(candidate1, wins.get(candidate1) + 1);
+        } else {
+            wins.put(candidate2, wins.get(candidate2) + 1);
+        }
     }
 }
